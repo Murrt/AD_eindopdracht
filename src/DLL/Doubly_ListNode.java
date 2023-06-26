4c0a5931b60c8446a1cd77cec89fc95dbb3ac010
Mart Hoff <pietertjepost123@gmail.com>
2023-06-26 17:29:04 +0200

    Update Doubly_ListNode.java
    DLL node extend nu  LL node

--- src/DLL/Doubly_ListNode.java
+++ src/DLL/Doubly_ListNode.java
@@ -1,31 +1,23 @@
 package DLL;
-public class Doubly_ListNode {
 
-    public int data;
-    public Doubly_ListNode prev;
-    public Doubly_ListNode next;
-    public Doubly_ListNode head;
+public class Doubly_ListNode extends LL.ListNode<Integer> {
+
+    public Doubly_ListNode prev, next, tail, head;
 
     public Doubly_ListNode() {
-        this.data = 0;
-        this.prev = null;
-        this.next = null;
+        super();
     }
 
     public Doubly_ListNode(Integer data) {
-        this.data = data;
-        this.prev = null;
-        this.next = null;
+        super(data);
     }
 
     public Doubly_ListNode(Integer data, Doubly_ListNode next, Doubly_ListNode prev) {
-        this.data = data;
+        super(data, next);
         this.prev = prev;
-        this.next = next;
     }
 
     public String toString() {
         return "Doubly_ListNode(" + data + ")";
     }
-
 }
