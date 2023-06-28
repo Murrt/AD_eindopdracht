9727cbcd5410fec16c143fc4f4b1bfd5764afc1d
Gharyan <simon_98@live.nl>
2023-06-28 23:40:17 +0200

    Update main.java

--- src/main.java
+++ src/main.java
@@ -1,19 +1,20 @@
 
+import BT.Binary_Search_Tree;
 import DLL.Doubly_Linked_List;
 
 public class main {
 	public static void main(String[] args) throws Exception {
 
-		Doubly_Linked_List d_linkedlist = new Doubly_Linked_List();
-		d_linkedlist.add(5);
-		d_linkedlist.add(2);
-		d_linkedlist.add(3);
-		d_linkedlist.add(4);
+		// Doubly_Linked_List d_linkedlist = new Doubly_Linked_List();
+		// d_linkedlist.add(5);
+		// d_linkedlist.add(2);
+		// d_linkedlist.add(3);
+		// d_linkedlist.add(4);
 
-		// d_linkedlist.simpleSearch(2);
+		// // d_linkedlist.simpleSearch(2);
 
-		String testy = d_linkedlist.print_nodes();
-		System.out.println(testy);
+		// String testy = d_linkedlist.print_nodes();
+		// System.out.println(testy);
 
 		// d_linkedlist.simpleSort();
 		// System.out.println("---------------------");
@@ -25,6 +26,10 @@ public class main {
 		// sorted.print_nodes();
 
 		// d_linkedlist.fastSearch(3);
+
+		Binary_Search_Tree bst = new Binary_Search_Tree();
+		
+		bst.addNode("hello");
 
 	}
 
